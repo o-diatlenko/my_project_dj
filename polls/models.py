fc2668a3a2cfa7dffd89474e329552867a80dcd3
Kate Brushkova <ekaterinabruskova7@gmail.com>
2021-02-02 12:54:21 +0200

    [ADD] create the admin site

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(verbose_name='date published')
 
     def __str__(self):
         return self.question_text
@@ -16,7 +16,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
